c1f50acc4eb65b6c03001cab89651d797df3e811
Menfi Systems <Menfi@Michaels-Mac-mini.local>
2016-11-01 10:23:39 -0500

    aggregation pipeline examples, includes multiple  fields

--- dataExtractionFundamentals/pythonSourceCode/aggregationFrameworkExamples.py
+++ dataExtractionFundamentals/pythonSourceCode/aggregationFrameworkExamples.py
@@ -101,12 +101,176 @@ pipeline.append({'$match' : {'population' : {'$gt' : 0}}})
 pipeline.append({'$unwind' : '$isPartOf'})
 pipeline.append({'$group' : {'_id' : '$isPartOf', 'regionPopAvg' : {'$avg' : '$population'}}})
 pipeline.append({ '$group' : { '_id' : 'null', 'avg' : { '$avg' : '$regionPopAvg'}}})
+
+# Most Common City Name
+pipeline.append({ '$match' : {'name' : {'$exists' : 1}} })
+pipeline.append({ '$group' : {  '_id' : '$name',  'count' : {  '$sum' : 1  }  }  })
+pipeline.append({  '$sort' : {'count' : -1 }  })
+pipeline.append({  "$limit" : 1 })
+
+
+# Region Cities Quiz
+pipeline.append({'$match' : {'country' : 'India'} } )
+pipeline.append({ '$match' : {'name' : {'$exists' : 1}} })
+
+# pipeline.append( {"$match": {'lon': {'$gt': 75, '$lt': 80} }} )
+# BOTH WORK above and below 
+pipeline.append({ '$match' : {'lon' : {'$gt' : 75}} })
+pipeline.append({ '$match' : {'lon' : {'$lt' : 80}} })
+
+pipeline.append({'$unwind' : '$isPartOf'})
+
+pipeline.append({ '$group' : {  '_id' : '$isPartOf',  'count' : {  '$sum' : 1  } }  })
+pipeline.append({  '$sort' : {'count' : -1 }  })
+pipeline.append({'$limit' : 1 })
+
     
- 
+    
+# Average Population 
+# Make sure we have the fields populated that we need 
+pipeline.append({ '$match' : {'country' : {'$exists' : 1}, } })
+pipeline.append({ '$match' : {'population' : {'$exists' : 1}, } })
+pipeline.append({ '$match' : {'name' : {'$exists' : 1}, } })
+pipeline.append({ '$match' : {'isPartOf' : {'$exists' : 1}, } })
+
+# get the array of interest unwound first 
+pipeline.append({'$unwind' : '$isPartOf'})
+
+# $group, more than one $group '_id' field, group by the 'isPartOf' filed WITHIN the country field - nested $group
+pipeline.append({"$group":{"_id":{"country":"$country","isPartOf": "$isPartOf"},
+                      "avg":{"$avg":"$population"},
+                      # 'popArray' : {'$push' : '$population'}
+                       }})
+           
+# use                        
+pipeline.append({"$group":{'_id':'$_id.country','avgRegionalPopulation':{'$avg':'$avg'}}})
 
 
+# Average Population - nested $group
+def make_pipeline():
+    # complete the aggregation pipeline
+    pipeline = [ ]
+
+    # used for testing, developemnt 
+    # pipeline.append( {"$match": {'country' : 'India'}  })  
+
+    # used for testing, developemnt 
+    # pipeline.append( {"$match": { '$or' : [{'country' : 'India'},
+    # {'country' : 'Iran'}] }  })
+
+    # used for testing, developemnt 
+    # pipeline.append( {"$match": { '$or' : [{'isPartOf' : 'Urmia County'},
+    # {'isPartOf' : 'Khordha district'}] }  })
+    
+    pipeline.append({ '$match' : {'country' : {'$exists' : 1}, } })
+    pipeline.append({ '$match' : {'population' : {'$exists' : 1}, } })
+    pipeline.append({ '$match' : {'name' : {'$exists' : 1}, } })
+    pipeline.append({ '$match' : {'isPartOf' : {'$exists' : 1}, } })
+    
+    # used for testing, developemnt
+    # pipeline.append( {"$match":{ 'isPartOf.2': { '$exists' : 1 } }})
+    
+    # associate the population field with EACH [region, district] in the isPartOf array 
+    pipeline.append({'$unwind' : '$isPartOf'})
+    
+    # requirement -
+    # first calculate the average city population for each [region, district] in a country
+    #
+    # first part of the assigment requires two $group
+    # 1.) $group isPartOf field within 2.) $group country field 
+    # $group by country, the outer grouping
+    # $group isPartOf inner grouping
+    # there are two and just as important only two required $group fields.
+    # additional $group fields will break it
+    # 
+    # average the population field now associated with EACH [region, district] field
+    #
+    # output multiple documents for each country
+    # one document per [region, district] with the associtaed population average
+    #
+    # *** Note multiple $group fields within the *** 
+    # {"_id":{"country":"$country" .......}
+    pipeline.append({"$group":{"_id":{"country":"$country","isPartOf": "$isPartOf"},
+                          "avg":{"$avg":"$population"},
+                          # 'popArray' : {'$push' : '$population'}
+                           }})
+                           
+    # sample output documents   
+    # Clear illustration - fields nested under the _id field 
+    # hence "_id." prefix dot notation required
+    # sample output
+    # Multiple documents for each country - see the second $group below  
+    # {u'_id': {u'country': u'Iran', u'isPartOf': u'Urmia County'}, u'avg': 255628.4},
+    # {u'_id': {u'country': u'Iran', u'isPartOf': u'Sumay-ye Beradust District'}, u'avg': 1508.0}
+    # {u'_id': {u'country': u'Iran', u'isPartOf': u'Silvaneh District'}, u'avg': 1350.0},
+      
+    # used for testing, developemnt
+    # fails missing _id dot notation prefix                        
+    # pipeline.append({'$match' : {'country' : 'India'}  }) 
+    
+    # used for testing, developemnt
+    # works!!! _id dot notation prefix required         
+    # pipeline.append({'$match' : {'_id.country' : 'India'}  })
+    
+    # used for testing, developemnt
+    # fails generic xx does NOT work
+    # pipeline.append({ '$project' : { 'xx' : '$_id.country'}})
+    
+    # used for testing, developemnt
+    # works _id dot notation required
+    # pipeline.append({ '$project' : { '_id.country' : '$_id.country'}})
+
+    # used for testing, developemnt
+    # works _id dot notation required
+    # pipeline.append({ '$project' : { '_id.isPartOf' : '$_id.isPartOf'}})
+    
+    # used for testing, developemnt
+    # works
+    # dot notation required
+    # pipeline.append({ '$project' : { '_id.country' : '$_id.country',
+    # '_id.isPartOf' : '$_id.isPartOf'   }})
+    
+    # used for testing, developemnt
+    # works
+    # _id prefix required on the left
+    # dot notation required
+    # pipeline.append({ '$project' : { '_id.avg' : '$avg'}})
+    
+    # used for testing, developemnt
+    # fails elevation NOT part of the $group above
+    # pipeline.append({ '$project' : { '_id.isPartOf' : '$_id.elevation'}})
+    
+    # second part of the assignmnet 
+    # requirement - 
+    # then calculate the average of all the regional averages for a country
+    #
+    # sample input documents - note multiple documnets for each country
+    # $group by country required to meet requiremnt 
+    # {u'_id': {u'country': u'Iran', u'isPartOf': u'Urmia County'}, u'avg': 255628.4},
+    # {u'_id': {u'country': u'Iran', u'isPartOf': u'Sumay-ye Beradust District'}, u'avg': 1508.0}
+    # {u'_id': {u'country': u'Iran', u'isPartOf': u'Silvaneh District'}, u'avg': 1350.0},
+    #
+    # the first left $avg is the airthmetic operator - 'average'
+    # the second right $avg is value that is $group by country then averaged
+    # {'$avg':'$avg'}
+    #
+    # *** Only 1 $group field - country  *** 
+    pipeline.append({"$group":{'_id':'$_id.country','avgRegionalPopulation':{'$avg':'$avg'}}})
+    # sample output 1 document for each country  
+    # {u'_id': u'Lithuania', u'avgRegionalPopulation': 14750.784447977203}
+    # {u'_id': u'Iran', u'avgRegionalPopulation': 31174.275647889808}
 
 
+# misc
+
+# match equals 
+pipeline.append({'$match' : {'country' : 'India'} } ) #gets many
+
+# match not equals
+pipeline.append({'$match' : { 'country' : { '$ne' : 'India'} }  }) # gets many
+
+# match '$or'
+pipeline.append( {"$match": { '$or' : [{'country' : 'Bolivia'}, {'country' : 'Sudan'}] }  })
 
 
 
